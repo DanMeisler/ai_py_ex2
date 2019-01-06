d6677875f65ff7d64a8757b5f74c5a4ed3ad3526
DanMeisler <danmoshem@gmail.com>
2019-01-06 12:41:38 +0200

    * load_table - cluster attribute is the last column * add hamming distance function

--- py_ex2.py
+++ py_ex2.py
@@ -1,5 +1,3 @@
-CLUSTERING_ATTRIBUTE = "survived"
-
 
 def load_table(table_file_path):
     table = []
@@ -7,14 +5,23 @@ def load_table(table_file_path):
         attributes_names = table_file.readline().split()
         for data_line in table_file.readlines():
             data_line_dictionary = dict(zip(attributes_names, data_line.split()))
-            cluster = data_line_dictionary.pop(CLUSTERING_ATTRIBUTE)
+            _, cluster = data_line_dictionary.popitem()
             table.append((data_line_dictionary, cluster))
     return table
+
+
+def calculate_hamming_distance(dictionary1, dictionary2):
+    hamming_distance = 0
+    for attribute_name in dictionary1.keys():
+        if dictionary1[attribute_name] != dictionary2[attribute_name]:
+            hamming_distance += 1
+    return hamming_distance
 
 
 def main():
     table = load_table("train.txt")
     print(table)
+    print(calculate_hamming_distance(table[0][0], table[3][0]))
 
 
 if __name__ == "__main__":
