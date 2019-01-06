4fd867fd1dd349abe8d0be983ed36f6ad3c9440f
DanMeisler <danmoshem@gmail.com>
2019-01-06 14:29:43 +0200

    Write an output file. keep implementing

--- py_ex2.py
+++ py_ex2.py
@@ -1,4 +1,11 @@
+from itertools import zip_longest
 from collections import Counter
+
+
+class ModelTypes:
+    DECISION_TREE = 0
+    KNN = 1
+    NAIVE_BASE = 2
 
 
 def load_table(table_file_path):
@@ -6,8 +13,8 @@ def load_table(table_file_path):
     Loads table from a file.
     The resulting table form is [(instance1, class_name), (instance2, class_name), ...]
     where each instance is a dictionary of the attributes.
-    :param table_file_path: the path to the file to be loaded
-    :return: The table
+    :param table_file_path: path to the file to be loaded
+    :return: a table
     """
     table = []
     with open(table_file_path, "r") as table_file:
@@ -33,24 +40,74 @@ def calculate_hamming_distance(instance1, instance2):
     return hamming_distance
 
 
-def knn_classify(table, instance, k=5):
+def knn_classify(training_table, instance, k=5):
     """
-    Classify instance given a table.
-    :param table: A table (See load_table function doc for more information)
-    :param instance: An instance (See load_table function doc for more information)
+    Classify instance given a training table.
+    :param training_table: a table (See load_table function doc for more information)
+    :param instance: an instance (See load_table function doc for more information)
     :param k: the k parameter in the knn algorithm
     :return: the instance class_name
     """
-    knn = sorted(table, key=lambda x: calculate_hamming_distance(x[0], instance))[:k]
+    knn = sorted(training_table, key=lambda x: calculate_hamming_distance(x[0], instance))[:k]
     knn_class_name_counter = Counter(map(lambda x: x[1], knn))
     return sorted(knn_class_name_counter, key=knn_class_name_counter.get)[-1]
 
 
+def create_prediction(training_table, instances, model_type):
+    """
+    Create prediction given a training table, instances to predict and model type to use.
+    Returned prediction is in the form of [instance1's class_name, instance2's class_name, ...]
+    :param training_table: a table (See load_table function doc for more information)
+    :param instances: instances to be predicted (See load_table function doc for more information)
+    :param model_type: a ModelTypes value
+    :return: a prediction
+    """
+    prediction = []
+    for instance in instances:
+        if model_type == ModelTypes.KNN:
+            prediction.append(knn_classify(training_table, instance))
+        else:
+            raise NotImplementedError()
+    return prediction
+
+
+def calculate_prediction_accuracy(prediction, real):
+    """
+    Calculates the accuracy of a prediction.
+    :param prediction: a prediction (See create_prediction function doc for more information)
+    :param real: a prediction like structure containing the real data
+    :return: the accuracy the prediction is write
+    """
+    return sum(x == y for x, y in zip(prediction, real)) / float(len(prediction))
+
+
+def output_prediction(training_table, test_table, output_file_path="output.txt"):
+    """
+    Creates output file given both training and test tables.
+    :param training_table: a table (See load_table function doc for more information)
+    :param test_table: a table (See load_table function doc for more information)
+    :param output_file_path: path to a file to write the output into
+    """
+    header_line = "\t".join(["Num", "DT ", "KNN>", "naiveBase"])
+    dt_prediction = []
+    knn_prediction = create_prediction(training_table, list(map(lambda x: x[0], test_table)), ModelTypes.KNN)
+    knn_accuracy = calculate_prediction_accuracy(knn_prediction, list(map(lambda x: x[1], test_table)))
+    naive_base_prediction = []
+    accuracies = ("0", str(knn_accuracy), "0")
+    with open(output_file_path, "w") as output_file:
+        output_file.write(header_line + "\n")
+        i = 1
+        for instance_prediction in zip_longest(dt_prediction, knn_prediction, naive_base_prediction, fillvalue=""):
+            output_file.write("%d\t" % i + "\t".join(instance_prediction) + "\n")
+            i += 1
+
+        output_file.write("\t" + "\t".join(accuracies) + "\n")
+
+
 def main():
-    table = load_table("train.txt")
-    print(table)
-    print(calculate_hamming_distance(table[0][0], table[3][0]))
-    print(knn_classify(table, table[25][0]))
+    training_table = load_table("train.txt")
+    test_table = load_table("test.txt")
+    output_prediction(training_table, test_table)
 
 
 if __name__ == "__main__":
