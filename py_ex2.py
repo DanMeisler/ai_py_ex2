0b7f5cac16906fbd2a2ca2a0b16711e3ad814638
DanMeisler <danmoshem@gmail.com>
2019-01-14 22:36:17 +0200

    Some improvements, add doc

--- py_ex2.py
+++ py_ex2.py
@@ -25,6 +25,76 @@ def load_table(table_file_path):
     return table
 
 
+def get_classes_names(table):
+    """
+    :param table: a table (See load_table function doc for more information)
+    :return all classed names based on the table
+    """
+    return set(map(lambda x: x[1], table))
+
+
+def get_possible_attribute_values(table, attribute_name):
+    """
+    :param table: a table (See load_table function doc for more information)
+    :param attribute_name: the attribute's name
+    :return: all the possible values for attribute the specified attribute
+    """
+    return set(map(lambda x: x[0][attribute_name], table))
+
+
+def get_instance_count(table, filter_function):
+    """
+    :param table: a table (See load_table function doc for more information)
+    :param filter_function: function to filter instances with
+    :return: not filter matching instance count
+    """
+    return len(list(filter(filter_function, table)))
+
+
+def get_smoothed_attribute_probability(table, class_name, attribute_name, attribute_value):
+    """
+    :param table: a table (See load_table function doc for more information)
+    :param class_name: a class name
+    :param attribute_name: an attribute name
+    :param attribute_value: an attribute value
+    :return: smoothed probability of an attribute
+    """
+    k = len(get_possible_attribute_values(table, attribute_name))
+    n1 = get_instance_count(table, lambda x: ((x[1] == class_name) and (x[0][attribute_name] == attribute_value)))
+    n2 = get_instance_count(table, lambda x: x[1] == class_name)
+    return (n1 + 1) / float(n2 + k)
+
+
+def get_class_probability(table, class_name):
+    return get_instance_count(table, lambda x: x[1] == class_name) / float(len(table))
+
+
+def get_smoothed_probability(table, instance, class_name):
+    """
+    :param table:  a table (See load_table function doc for more information)
+    :param instance: an instance
+    :param class_name: a class name
+    :return: smoothed probability
+    """
+    smoothed_attributes_probabilities = \
+        [get_smoothed_attribute_probability(table, class_name, x, y) for x, y in instance.items()]
+    return reduce(operator.mul, smoothed_attributes_probabilities, get_class_probability(table, class_name))
+
+
+def get_hamming_distance(instance1, instance2):
+    """
+    Given two table instances (dictionaries), returns the hamming distance between them.
+    :param instance1: first instance (See load_table function doc for more information)
+    :param instance2: second instance (See load_table function doc for more information)
+    :return: Hamming distance
+    """
+    hamming_distance = 0
+    for attribute_name in instance1.keys():
+        if instance1[attribute_name] != instance2[attribute_name]:
+            hamming_distance += 1
+    return hamming_distance
+
+
 class DecisionTreeNode(object):
     def __init__(self, class_name=None):
         self.attribute_name = None
@@ -44,7 +114,6 @@ class DecisionTree(object):
 
     def __init__(self, table):
         self._table = table
-        self._default_class_name = list(get_classes_names(table))[0]
         self._root = None
         self._build()
         self._save_to_file()
@@ -60,6 +129,10 @@ class DecisionTree(object):
         return "\n".join(lines)
 
     def classify(self, instance):
+        """
+        :param instance: an instance (See load_table function doc for more information)
+        :return: the instance class_name
+        """
         node = self._root
         while not node.class_name:
             for child in node.children:
@@ -69,8 +142,11 @@ class DecisionTree(object):
         return node.class_name
 
     def _build(self):
+        """
+        Build the decision tree using the id3 algorithm
+        """
         attributes_names = list(self._table[0][0])
-        self._root = self._run_dtl(self._table, attributes_names)
+        self._root = self._run_dtl(self._table, attributes_names, self._mode(self._table))
 
     @staticmethod
     def compute_entropy(table):
@@ -93,26 +169,34 @@ class DecisionTree(object):
     def _dtl_best_attribute(table, attributes_names):
         return max(attributes_names, key=lambda x: DecisionTree.compute_gain(table, x))
 
-    def _run_dtl(self, table, attributes_names):
-        if len(table) == 0:
-            return DecisionTreeNode( self._default_class_name)
-        if set(map(lambda x: x[1], table)) == 1:
+    @staticmethod
+    def _mode(table):
+        return max(get_classes_names(table), key=lambda x: get_class_probability(table, x))
+
+    @staticmethod
+    def _run_dtl(table, attributes_names, default):
+        """
+        Recursive function which implements the id3 algorithm
+        """
+        if len(table) == 0:  # no examples left
+            return DecisionTreeNode(default)
+        if set(map(lambda x: x[1], table)) == 1:  # all examples have same classification
             leaf_node = DecisionTreeNode()
             leaf_node.class_name = table[0][1]
             return leaf_node
-        if len(attributes_names) == 0:
+        if len(attributes_names) == 0:  # no attributes left
             leaf_node = DecisionTreeNode()
-            leaf_node.class_name = max(get_classes_names(table), key=lambda x: get_class_probability(table, x))
+            leaf_node.class_name = DecisionTree._mode(table)
             return leaf_node
 
         node = DecisionTreeNode()
-        attribute_name = self._dtl_best_attribute(table, attributes_names)
+        attribute_name = DecisionTree._dtl_best_attribute(table, attributes_names)
 
         for attribute_value in get_possible_attribute_values(table, attribute_name):
             child_node_table = list(filter(lambda x: x[0][attribute_name] == attribute_value, table))
             child_node_attributes_names = attributes_names[:]
             child_node_attributes_names.remove(attribute_name)
-            child_node = self._run_dtl(child_node_table, child_node_attributes_names)
+            child_node = DecisionTree._run_dtl(child_node_table, child_node_attributes_names, DecisionTree._mode(table))
             child_node.attribute_name = attribute_name
             child_node.attribute_value = attribute_value
             node.children.append(child_node)
@@ -125,23 +209,9 @@ class DecisionTree(object):
             save_file.write(str(self))
 
 
-def get_hamming_distance(instance1, instance2):
-    """
-    Given two table instances (dictionaries), returns the hamming distance between them.
-    :param instance1: first instance (See load_table function doc for more information)
-    :param instance2: second instance (See load_table function doc for more information)
-    :return: Hamming distance
-    """
-    hamming_distance = 0
-    for attribute_name in instance1.keys():
-        if instance1[attribute_name] != instance2[attribute_name]:
-            hamming_distance += 1
-    return hamming_distance
-
-
 def knn_classify(training_table, instance, k=5):
     """
-    Classify instance given a training table.
+    Classify instance using knn algorithm given a training table.
     :param training_table: a table (See load_table function doc for more information)
     :param instance: an instance (See load_table function doc for more information)
     :param k: the k parameter in the knn algorithm
@@ -152,38 +222,20 @@ def knn_classify(training_table, instance, k=5):
     return sorted(knn_class_name_counter, key=knn_class_name_counter.get)[-1]
 
 
-def get_classes_names(table):
-    return set(map(lambda x: x[1], table))
-
-
-def get_possible_attribute_values(table, attribute_name):
-    return set(map(lambda x: x[0][attribute_name], table))
-
-
-def get_instance_count(table, filter_function):
-    return len(list(filter(filter_function, table)))
-
-
-def get_smoothed_attribute_probability(table, class_name, attribute_name, attribute_value):
-    k = len(get_possible_attribute_values(table, attribute_name))
-    n1 = get_instance_count(table, lambda x: ((x[1] == class_name) and (x[0][attribute_name] == attribute_value)))
-    n2 = get_instance_count(table, lambda x: x[1] == class_name)
-    return (n1 + 1) / float(n2 + k)
-
-
-def get_class_probability(table, class_name):
-    return get_instance_count(table, lambda x: x[1] == class_name) / float(len(table))
-
-
-def get_smoothed_probability(table, instance, class_name):
-    smoothed_attributes_probabilities = \
-        [get_smoothed_attribute_probability(table, class_name, x, y) for x, y in instance.items()]
-    return reduce(operator.mul, smoothed_attributes_probabilities, get_class_probability(table, class_name))
-
-
 def naive_base_classify(training_table, instance):
-    # TODO - what in the case of equal probability
-    return max(get_classes_names(training_table), key=lambda x: get_smoothed_probability(training_table, instance, x))
+    """
+    Classify instance using naive base given a training table.
+    :param training_table: a table (See load_table function doc for more information)
+    :param instance: an instance (See load_table function doc for more information)
+    :return: the instance class_name
+    """
+    classes_smoothed_probabilities = \
+        {x: get_smoothed_probability(training_table, instance, x) for x in get_classes_names(training_table)}
+
+    if len(set(classes_smoothed_probabilities.values())) == 1:  # in case all class have same probability
+        return max(get_classes_names(training_table), key=lambda x: get_class_probability(training_table, x))
+
+    return max(classes_smoothed_probabilities, key=classes_smoothed_probabilities.get)
 
 
 def create_prediction(training_table, instances, model_type):
@@ -206,7 +258,7 @@ def create_prediction(training_table, instances, model_type):
     raise NotImplementedError()
 
 
-def get_prediction_accuracy(prediction, real):
+def compute_prediction_accuracy(prediction, real):
     """
     Calculates the accuracy of a prediction.
     :param prediction: a prediction (See create_prediction function doc for more information)
@@ -219,10 +271,6 @@ def get_prediction_accuracy(prediction, real):
     return round(sum(x == y for x, y in zip(prediction, real)) / float(len(prediction)), 2)
 
 
-def get_table_instances(table):
-    return list(map(lambda x: x[0], table))
-
-
 def output_predictions(training_table, test_table, output_file_path="output.txt"):
     """
     Creates output file given both training and test tables.
@@ -231,10 +279,10 @@ def output_predictions(training_table, test_table, output_file_path="output.txt"
     :param output_file_path: path to a file to write the output into
     """
     header_line = "\t".join(["Num", "DT ", "KNN>", "naiveBase"])
-    test_instances = get_table_instances(test_table)
+    test_instances = list(map(lambda x: x[0], test_table))
     predictions = list(map(lambda x: create_prediction(training_table, test_instances, x), MODEL_TYPES))
     real = list(map(lambda x: x[1], test_table))
-    predictions_accuracies = map(lambda x: str(get_prediction_accuracy(x, real)), predictions)
+    predictions_accuracies = map(lambda x: str(compute_prediction_accuracy(x, real)), predictions)
 
     with open(output_file_path, "w") as output_file:
         output_file.write(header_line + "\n")
